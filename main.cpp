7d6d3200ea20f1f7d57e3a71cb06670d4490db9a
Nazeeruddin <naseers.tech@gmail.com>
2022-08-11 13:34:29 +0530

    age variable added

--- main.cpp
+++ main.cpp
@@ -7,5 +7,10 @@ int main()
     std::string str;
     cout<<"Enter your name: ";
     cin>>str;
+
+    int age;
+    cout<<"Enter your age: ";
+    cin>>age;
     std::cout<<"Hello "<<str<<"!!!"<<std::endl;
+    std::cout<<"Your age is "<<age<<std::endl;
 }
